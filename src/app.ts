c874263efbcdcec4dace3ad0578202b746707062
Nikita Legkiy Maxakov <iamlegkiy@gmail.com>
2023-02-14 22:12:41 +0800

    added 2 new way to get data

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import axios from 'axios';
 import { parse } from 'fast-csv';
-import { getCsvData } from './utils/dataFetch';
+import { getCsvData, nativeGetData, nativeGetData2 } from './utils/dataFetch';
 interface ICSVData {
   date: string;
   time: string;
@@ -21,14 +21,14 @@ const host = 'localhost';
 
 // app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.use('/day', async (req: Request, res: Response) => {
+  nativeGetData2();
+  res.json({ data: nativeGetData2() });
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.json({ greeting: 'Hello world!' });
 });
-
-app.get('/day', (req: Request, res: Response) => {
-  getCsvData();
-});
-
 app.listen(port, () => {
   console.log(`Server start on: http://${host}:${port}`);
 });
--- src/utils/dataFetch.ts
+++ src/utils/dataFetch.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import { parse } from 'fast-csv';
+import readline from 'readline';
+
+const dataWay = './public/2022-09.csv';
 
 export const getCsvData = () => {
-  fs.createReadStream('./public/2022-09.csv')
+  fs.createReadStream(dataWay)
     .pipe(parse({ headers: true }))
     .on('data', function (row) {
       console.log(row);
@@ -10,4 +13,23 @@ export const getCsvData = () => {
     .on('error', function (error) {
       console.log(error.message);
     });
+};
+
+export const nativeGetData = fs.readFile(dataWay, 'utf8', (err, data) => data);
+
+export const nativeGetData2 = () => {
+  const stream = fs.createReadStream(dataWay);
+  const reader = readline.createInterface({ input: stream });
+  let data: any = [];
+
+  reader.on('line', (row: any) => {
+    // 👇 split a row string into an array
+    // then push into the data array
+    data.push(row.split(','));
+  });
+
+  reader.on('close', () => {
+    // 👇 reached the end of file
+    console.log(data);
+  });
 };
