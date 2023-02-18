9f4419e5e1cd9995ec2a587b9aa86e718e446332
Nikita Legkiy Maxakov <iamlegkiy@gmail.com>
2023-02-18 21:25:29 +0800

    try to make ip cam out - false, connected and test respons to frontend

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from 'express';
+import { PORT } from '../config';
 import path from 'path';
 import fs from 'fs';
 import axios from 'axios';
 import { parse } from 'fast-csv';
 import { getData, nativeGetData, nativeGetData2 } from './utils/dataFetch';
 import setCors from './middlewares/cors';
+//@ts-ignore
+import Stream from 'node-rtsp-stream';
 interface ICSVData {
   date: string;
   time: string;
@@ -17,18 +20,31 @@ interface ICSVData {
 }
 const app: Express = express();
 
-const port: string = process.env.PORT || '8484';
+const port: number = PORT || 8484;
 const host = 'localhost';
-
+const source = 'rtsp://admin:Password@192.168.31.53:554/';
 app.use(setCors);
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ greeting: 'Hello world!' });
-});
+// const stream = new Stream({
+//   name: 'name',
+//   streamUrl: source,
+//   wsPort: 9999,
+//   ffmpegOptions: {
+//     // options ffmpeg flags
+//     '-stats': '', // an option with no neccessary value uses a blank string
+//     '-r': 30, // options with required values specify the value after the key
+//   },
+// });
 
 app.use('/day', async (req: Request, res: Response) => {
-  //@ts-ignore
   res.json(getData(0));
+});
+app.use('/test', (req: Request, res: Response) => {
+  res.json({ test: 'from back' });
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ greeting: 'Server start on PORT  = ' + port });
 });
 
 app.listen(port, () => {
